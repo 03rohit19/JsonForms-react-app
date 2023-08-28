e6169faf4123382c7389708702af59e4d72a962a
ROHIT <125283790+03rohit19@users.noreply.github.com>
2023-08-28 17:41:14 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import JsonFormsScreen from "./components/JsonFormsScreen";
-import { createMuiTheme } from "@mui/material";
+
 
 export const App = () => <JsonFormsScreen />;
 
 export default App;
 
-export const customizedTheme = createMuiTheme({
-  jsonforms: { input: { category: { color: "#FFFF00" } } },
-});
+
